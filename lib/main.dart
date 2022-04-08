3454c8beeba243c9beedb60d236d9d5abe152840
EasiCode <abiodun.ajibade@rocketmail.com>
2022-04-08 10:46:44 +0200

    client socket

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -48,6 +51,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  Future<void> _initClient() async {
+    // connect to the socket server
+      final socket = await Socket.connect('127.0.0.1', 18910);
+      print(
+          'Connected to Server: ${socket.remoteAddress.address}:${socket.remotePort}');
+
+      // listen for responses from the server
+      socket.listen(
+        // handle data from the server
+        (Uint8List data) {
+          final serverResponse = String.fromCharCodes(data);
+          print('Server: $serverResponse');
+        },
+
+        // handle errors
+        onError: (_) {
+          print('Connection Error: $_');
+          socket.destroy();
+        },
+
+        // handle server ending connection
+        onDone: () {
+          print('Server left.');
+          socket.destroy();
+        },
+      );
+      // send some messages to the server
+  await sendMessage(socket, 'Knock, knock.');
+  await sendMessage(socket, 'client');
+  await sendMessage(socket, 'client');
+  await sendMessage(socket, 'OK, it is enough Mr. server. Stop asking who hehe');
+  }
+  Future<void> sendMessage(Socket socket, String message) async {
+  print('Client: $message');
+  socket.write(message);
+  await Future.delayed(Duration(seconds: 2));
+}
+
+void initState() {
+  _initClient();
+}
   int _counter = 0;
 
   void _incrementCounter() {
